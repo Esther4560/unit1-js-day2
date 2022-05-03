9eba174c7b0da2b67222e4476ea26f42621263e0
Esther4560 <Esthernoms@gmail.com>
2022-05-03 15:51:52 +0200

    JS  coding

---
+++ assignment.js
@@ -0,0 +1,89 @@
+/* EXERCISE 1
+ Enumerate and describe the main datatypes in JavaScript. Try to explain the concepts as you were speaking to a 12 years old.
+*/
+
+/* Data types basically specify what kind of data can be stored and manipulated within a program.
+There are six basic data types in JavaScript which can be divided into three main categories: 
+primitive (or primary), composite (or reference), and special data types. String, Number, and Boolean are primitive data types. 
+Object, Array, and Function (which are all types of objects) are composite data types. 
+Whereas Undefined and Null are special data types. 
+
+The String Data Type
+
+The string data type is used to represent textual data (i.e. sequences of characters). 
+Strings are created using single or double quotes surrounding one or more characters.
+example 
+var a = 'Hi there!';  // using single quotes
+var b = "Hi there!";  // using double quotes
+
+The Number Data Type
+The number data type is used to represent positive or negative numbers with or without decimal place, 
+or numbers written using exponential notation e.g. 1.5e-4 (equivalent to 1.5x10-4).
+example
+var a = 25;         // integer
+var b = 80.5;       // floating-point number
+var c = 4.25e+6;    // exponential notation, same as 4.25e6 or 4250000
+var d = 4.25e-6;    // exponential notation, same as 0.00000425 
+
+The Boolean Data Type
+The Boolean data type can hold only two values: true or false. 
+It is typically used to store values like yes (true) or no (false), on (true) or off (false), etc. 
+as demonstrated below:
+var isReading = true;   // yes, I'm reading
+var isSleeping = false; // no, I'm not sleeping
+Boolean values also come as a result of comparisons in a program.
+The following example compares two variables and shows the result in an alert dialog box:
+var isReading = true;   // yes, I'm reading
+var isSleeping = false; // no, I'm not sleeping*/
+
+/* EXERCISE 2
+ Try to describe what a variable is, in your own words.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 3
+ Write the code to execute an addition (a sum) of the numbers 12 and 20.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 4
+ Create a variable named x and assign to it the number 12.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 5
+ Create a variable called name and assign to it the string "John Doe".
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 6
+ Execute a subtraction between the number 12 and the variable x you declared before (which is storing the value 12).
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 7
+ Create two variables: name1 and name2. Assign to name1 the string "john", and to name2 the string "John" (this one has a capital J!)
+ Verify that name1 is different from name2 (HINT: check their equality to be false).
+ Finally, verify that the equality between name1 and name2 becomes true if both are lowercase (without changing the value of name2).
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 8
+ Create a variable named x, and assign to it a numeric value less than 10.
+ Write the code to print the literal value of x (ex.: if x is 1, it should print "one", if x is 5 it should print "five" etc.).
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 9
+ [Extra] Assign a value to a variable using a ternary operator (this topic has not been covered during lesson yet,
+ try to search it by yourself, tomorrow morning we'll discuss it together!)
+*/
+
+/* WRITE YOUR ANSWER HERE */
